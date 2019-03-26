1e7d8ee2b04fa1f80b701af075e5e7a246a8dddc
Sebastian <mailman.sebastian@gmail.com>
2019-03-26 17:43:21 -0400

    timing changes

--- code/makeFiles.py
+++ code/makeFiles.py
@@ -1,6 +1,12 @@
 import sys, os
 import time
+import math
 from collections import defaultdict
+
+
+fullListFreq = {}
+
+
 def removeStops(essay,stopWords):
 	newEssay = []
 	for word in essay:
@@ -25,10 +31,13 @@ def createDictionaryForFile(file):
 			if word not in freq:
 				freq[word] = 0
 			freq[word] += 1
+			if word not in fullListFreq:
+				fullListFreq[word] = 0
+			fullListFreq[word] += 1
 			'''if word not in fullWordList:
 				fullWordList.append(word)'''
 	sortedDict = sorted(freq.items(), key=lambda item: item[1],reverse = True)
-	return sortedDict
+	return freq
 def getAllEssays():
 	dirrec1 = os.listdir("Data2014")
 	allEssays = []
@@ -53,6 +62,34 @@ def getAllEssays():
 					allEssays.append(currentEssay)
 					openEssay.close()
 	return allEssays
+def tfComputation(docFreqList, document):
+	tf = {}
+	length = len(document)
+	for word, count in docFreqList.items():
+		tf[word] = count/length
+	return tf
+def computeTDF(essayList):
+	n = len(essayList)
+	idf = {}
+	for l in essayList:
+		for word, count in l.items():
+			if count > 0:
+				if word not in idf:
+					idf[word] = 0
+				idf[word] += 1
+	
+	for word, v in idf.items():
+		idf[word] = math.log(n / float(v))
+	return idf
+def computeTFIDF(tf,idf):
+    tf_idf = dict.fromkeys(tf.keys(), 0)
+    for word, v in tf.items():
+        tf_idf[word] = v * idf[word]
+    return tf_idf
+
+
+
+
 
 def main():	
 	start = time.time()
@@ -61,55 +98,65 @@ def main():
 
 	#get List of all stop words
 	statement = "create stop word list"
-	print(statement+dotWord[:50-len(statement)], end =" ")
+	print(statement+dotWord[:50-len(statement)], end =" ",flush=True)
 	stopWords = createStopList()
 	end1 = time.time()
-	print("done")
+	print("DONE\n")
 	print("Time to load stop words  (seconds):\t",round(end1 - start,15),'\telap:\t',round(end1-start,15))
 	
 
 	#get 2d list of all words in all essays
 	statement = "Loading all Student Essays"
-	print(statement+dotWord[:50-len(statement)], end =" ")
+	print(statement+dotWord[:50-len(statement)], end =" ",flush=True)
 	allEssays = getAllEssays()
-	print("done")
+	print("DONE\n")
 	end2 = time.time()
 	print("Time to load all essays  (seconds):\t",round(end2 - start,15),'\telap:\t',round(end2-end1,15))
 	
 
 	#remove all of the stop words within each essay
 	statement = "Remove all stop words from Essays"
-	print(statement+dotWord[:50-len(statement)], end =" ")
+	print(statement+dotWord[:50-len(statement)], end =" ",flush=True)
 	for i in range(len(allEssays)):
 		allEssays[i] = removeStops(allEssays[i],stopWords)
-	print("done")
+	print("DONE\n")
 	end3 = time.time()
 	print("Time to remove stop words(seconds):\t",round(end3 - start,15),'\telap:\t',round(end3-end2,15))
 
 
 	#convert each essay into a list of (word,freq) tuples
 	statement = "Convert each essay to freqCounts"
-	print(statement+dotWord[:50-len(statement)], end =" ")
+	print(statement+dotWord[:50-len(statement)], end =" ",flush=True)
 	freqCounts = []
 	for essay in allEssays:
 		freqCounts.append(createDictionaryForFile(essay))
-	print("done")
+	print("DONE\n")
 	end4 = time.time()
 	print("Time to create freq count(seconds):\t",round(end4 - start,15),'\telap:\t',round(end4-end3,15))
 
+	#sortedDict = sorted(fullListFreq.items(), key=lambda item: item[1],reverse = True)
 
-	#create a list of all unique words used in the essays
-	statement = "Create list of unique words"
-	print(statement+dotWord[:50-len(statement)], end =" "),
-	fullWordList = []
-	for essay in freqCounts:
-		for word in essay:
-			if word[0] not in fullWordList:
-				fullWordList.append(word[0])
-	print("done")
+
+
+	#Get the IDF of all the terms
+	statement = "Get the IDF computation"
+	print(statement+dotWord[:50-len(statement)], end =" ",flush=True)
+	idf = computeTDF(freqCounts)
+	print("DONE\n")
 	end5 = time.time()
-	print("Time to find unique words(seconds):\t",round(end5 - start,15),'\telap:\t',round(end5-end4,15))
+	print("Time to create IDF       (seconds):\t",round(end5 - start,15),'\telap:\t',round(end5-end4,15))
 
+
+
+	#Get the IDF of all the terms
+	statement = "Get the TF computations"
+	print(statement+dotWord[:50-len(statement)], end =" ",flush=True)
+	tfComputations = []
+	for i in range(len(allEssays)):
+		tfComputations.append(tfComputation(freqCounts[i],allEssays[i]))
+	print("DONE\n")
+	end6 = time.time()
+	print("Time to create TF's      (seconds):\t",round(end6 - start,15),'\telap:\t',round(end6-end5,15))
 
 
 
@@ -117,15 +164,14 @@ def main():
 
 	count = 0
 	for essay in freqCounts:
-		for word in essay:
-			count +=1
+		count+=len(essay)
 	
 
 
 
 
 	print("amount of total words :\t",count)
-	print("Amount of unique words:\t",len(fullWordList))
+	print("Amount of unique word2:\t",len(fullListFreq))
 	print("essays checked        :\t",len(allEssays))
 	end = time.time()
 	print("Total Elapsed Time       (seconds):\t",round(end - start,15))
